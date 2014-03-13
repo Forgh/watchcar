334258f078b64091c6e83d825703e547868f203a
dkurzaj@gmail.com <dkurzaj@gmail.com@cf1d1d16-e840-6ff8-888e-e88bdf6d9097>
2014-03-13 11:32:29 +0000

    Inversion des commandes drift back Décommentaire de l'IP du client

--- trunk/WatchYourCar_Client/gen/com/example/watchyourcar/R.java
+++ trunk/WatchYourCar_Client/gen/com/example/watchyourcar/R.java
@@ -10,14 +10,6 @@ package com.example.watchyourcar;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int smart_watch_2_text_color_grey=0x7f060003;
-        public static final int smart_watch_2_text_color_orange=0x7f060004;
-        public static final int smart_watch_2_text_color_white=0x7f060005;
-        public static final int smart_watch_text_color_grey=0x7f060000;
-        public static final int smart_watch_text_color_orange=0x7f060001;
-        public static final int smart_watch_text_color_white=0x7f060002;
-    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -25,35 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050019;
-        public static final int activity_vertical_margin=0x7f05001a;
-        public static final int headset_pro_control_height=0x7f050002;
-        public static final int headset_pro_control_width=0x7f050001;
-        public static final int headset_pro_text_size=0x7f050000;
-        public static final int smart_watch_2_control_height=0x7f050012;
-        public static final int smart_watch_2_control_width=0x7f050011;
-        public static final int smart_watch_2_statusbar_height=0x7f050013;
-        public static final int smart_watch_2_text_size_large=0x7f050016;
-        public static final int smart_watch_2_text_size_medium=0x7f050017;
-        public static final int smart_watch_2_text_size_small=0x7f050018;
-        public static final int smart_watch_2_widget_height=0x7f050015;
-        public static final int smart_watch_2_widget_width=0x7f050014;
-        public static final int smart_watch_control_height=0x7f050004;
-        public static final int smart_watch_control_width=0x7f050003;
-        public static final int smart_watch_text_size_normal=0x7f05000b;
-        public static final int smart_watch_text_size_small=0x7f05000c;
-        /**  Badge only contain numbers and is language independant. 
-         */
-        public static final int smart_watch_text_size_widget_badge=0x7f050010;
-        public static final int smart_watch_text_size_widget_name=0x7f05000d;
-        public static final int smart_watch_text_size_widget_text=0x7f05000f;
-        public static final int smart_watch_text_size_widget_time=0x7f05000e;
-        public static final int smart_watch_widget_height_inner=0x7f050008;
-        public static final int smart_watch_widget_height_outer=0x7f050006;
-        public static final int smart_watch_widget_text_background_height=0x7f05000a;
-        public static final int smart_watch_widget_text_background_width=0x7f050009;
-        public static final int smart_watch_widget_width_inner=0x7f050007;
-        public static final int smart_watch_widget_width_outer=0x7f050005;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int arrow_down=0x7f020000;
@@ -64,70 +29,49 @@ public final class R {
         public static final int arrow_up=0x7f020005;
         public static final int arrow_upleft=0x7f020006;
         public static final int arrow_upright=0x7f020007;
-        public static final int badge_counter=0x7f020008;
-        public static final int headset_pro_cancel_icn=0x7f020009;
-        public static final int headset_pro_focus_xs_icn=0x7f02000a;
-        public static final int headset_pro_ok_icn=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
-        public static final int rotate_left=0x7f02000d;
-        public static final int rotate_right=0x7f02000e;
-        public static final int stop=0x7f02000f;
-        public static final int text_bg=0x7f020010;
-        public static final int widget_frame=0x7f020011;
+        public static final int ic_launcher=0x7f020008;
+        public static final int rotate_left=0x7f020009;
+        public static final int rotate_right=0x7f02000a;
+        public static final int stop=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f090016;
-        public static final int backwardButton=0x7f090005;
-        public static final int driftBackLeftButton=0x7f090007;
-        public static final int driftBackRightButton=0x7f090006;
-        public static final int driftLeftButton=0x7f090008;
-        public static final int driftRightButton=0x7f090009;
-        public static final int edit_message=0x7f090000;
-        public static final int forwardButton=0x7f090004;
-        public static final int leftButton=0x7f090002;
-        public static final int rightButton=0x7f090003;
-        public static final int rotateLeftButton=0x7f09000a;
-        public static final int rotateRightButton=0x7f09000b;
-        public static final int smart_watch_notification_widget_background=0x7f09000c;
-        public static final int smart_watch_notification_widget_text_background=0x7f09000d;
-        public static final int smart_watch_notification_widget_text_name=0x7f090010;
-        public static final int smart_watch_notification_widget_text_time=0x7f09000e;
-        public static final int smart_watch_notification_widget_text_title=0x7f09000f;
-        public static final int smart_watch_widget_custom_image=0x7f090011;
-        public static final int smart_watch_widget_custom_text_view=0x7f090012;
-        public static final int smart_watch_widget_event_counter_badge=0x7f090014;
-        public static final int smart_watch_widget_event_counter_text=0x7f090015;
-        public static final int smart_watch_widget_icon=0x7f090013;
-        public static final int stopButton=0x7f090001;
+        public static final int action_settings=0x7f08000c;
+        public static final int backwardButton=0x7f080005;
+        public static final int driftBackLeftButton=0x7f080007;
+        public static final int driftBackRightButton=0x7f080006;
+        public static final int driftLeftButton=0x7f080008;
+        public static final int driftRightButton=0x7f080009;
+        public static final int edit_message=0x7f080000;
+        public static final int forwardButton=0x7f080004;
+        public static final int leftButton=0x7f080002;
+        public static final int rightButton=0x7f080003;
+        public static final int rotateLeftButton=0x7f08000a;
+        public static final int rotateRightButton=0x7f08000b;
+        public static final int stopButton=0x7f080001;
     }
     public static final class layout {
         public static final int activity_address=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int smart_watch_notification_widget=0x7f030002;
-        public static final int smart_watch_widget=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        /**  At least one dummy resource is needed to satisfy aapt.
-    Otherwise it complains about no res folder. 
-         */
-        public static final int app_name=0x7f040000;
-        public static final int backward=0x7f040005;
-        public static final int button_send=0x7f040003;
-        public static final int driftBackLeft=0x7f04000a;
-        public static final int driftBackRight=0x7f040009;
-        public static final int driftLeft=0x7f04000b;
-        public static final int driftRight=0x7f04000c;
-        public static final int edit_message=0x7f040002;
-        public static final int forward=0x7f040004;
-        public static final int left=0x7f040006;
-        public static final int right=0x7f040007;
-        public static final int rotateLeft=0x7f04000d;
-        public static final int rotateRight=0x7f04000e;
-        public static final int stop=0x7f040008;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int backward=0x7f050005;
+        public static final int button_send=0x7f050003;
+        public static final int driftBackLeft=0x7f05000a;
+        public static final int driftBackRight=0x7f050009;
+        public static final int driftLeft=0x7f05000b;
+        public static final int driftRight=0x7f05000c;
+        public static final int edit_message=0x7f050002;
+        public static final int forward=0x7f050004;
+        public static final int left=0x7f050006;
+        public static final int right=0x7f050007;
+        public static final int rotateLeft=0x7f05000d;
+        public static final int rotateRight=0x7f05000e;
+        public static final int stop=0x7f050008;
     }
     public static final class style {
         /** 
@@ -151,10 +95,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
