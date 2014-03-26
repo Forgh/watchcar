c2fd5b2302d5c0b158a5844f27f0df93af39a7c9
dkurzaj@gmail.com <dkurzaj@gmail.com@cf1d1d16-e840-6ff8-888e-e88bdf6d9097>
2014-03-26 14:14:30 +0000

    Le serveur est désormais géré à l'aide d'un thread afin de tourner à l'infini. Il est mono thread (threadPool = 0)

--- WatchYourCar_Server/src/Main.java
+++ WatchYourCar_Server/src/Main.java
@@ -1,8 +1,8 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import jssc.SerialPort;
 import jssc.SerialPortException;
@@ -11,81 +11,49 @@ import jssc.SerialPortList;
 public class Main {
 
     public static void main(String[] args) {
-	    try {
-	    	// Init server
-	    	ServerSocket serv_soc;
-			serv_soc = new ServerSocket(6020);
-			Socket soc = serv_soc.accept();
-			
-			BufferedReader ins = new BufferedReader(new	InputStreamReader(soc.getInputStream()));
+    	ServerSocket serv_soc = null;
+    	
+    	ExecutorService es;
+		es = Executors.newFixedThreadPool(1);
 		
-			// Init serial port
-			SerialPort serialPort = new SerialPort(SerialPortList.getPortNames()[0]);
+		// Init serial port
+		SerialPort serialPort = new SerialPort(SerialPortList.getPortNames()[0]);
+		try {
 			serialPort.openPort();
-			//Open serial port
-	        serialPort.setParams(SerialPort.BAUDRATE_115200, 
-	                             SerialPort.DATABITS_8,
-	                             SerialPort.STOPBITS_1,
-	                             SerialPort.PARITY_NONE);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
-	        serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
-	        
-	        int i = 0;
-	        while (i < 100000) {
-	        	String request = ins.readLine();
-	        	System.out.println("Request :"+request);
-	        	if (request != null) {
-		        	if (request.equals("left")) {
-		        		serialPort.writeBytes("4".getBytes());//Write data to port
-		        	}
-		        	else if (request.equals("right")) {
-		        		serialPort.writeBytes("6".getBytes());
-		        	}
-		        	else if (request.equals("stop")) {
-		        		serialPort.writeBytes("5".getBytes());
-		        	}
-		        	else if (request.equals("forward")) {
-		        		serialPort.writeBytes("8".getBytes());
-		        	}
-		        	else if (request.equals("backward")) {
-		        		serialPort.writeBytes("2".getBytes());
-		        	}
-		        	else if (request.equals("drift back right")) {
-		        		serialPort.writeBytes("1".getBytes());
-		        	}
-		        	else if (request.equals("drift back left")) {
-		        		serialPort.writeBytes("3".getBytes());
-		        	}
-		        	else if(request.equals("drift left")) {
-		        		serialPort.writeBytes("7".getBytes());
-		        	}
-		        	else if(request.equals("drift right")) {
-		        		serialPort.writeBytes("9".getBytes());
-		        	}
-		        	else if(request.equals("rotate left")) {
-		        		serialPort.writeBytes("0".getBytes());
-		        	}
-		        	else if(request.equals("rotate right")) {
-		        		serialPort.writeBytes(".".getBytes());
-		        	}
-		        	else {
-		        		System.out.println("Unknown command");
-		        	}
-	        	}
-	        	else {
-	        		i++;
-	        	}
+			serialPort.setParams(SerialPort.BAUDRATE_115200, 
+                             SerialPort.DATABITS_8,
+                             SerialPort.STOPBITS_1,
+                             SerialPort.PARITY_NONE);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
+			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+			
+		    // Init server
+			serv_soc = new ServerSocket(6020);
+			while (true) {
+				Socket soc = serv_soc.accept();
+				es.execute(new Connection(soc,serialPort));
 			}
-	        
-	        serialPort.closePort();
-	        ins.close();
-	        soc.close();
-	        serv_soc.close();
-	    }
+		}
         catch (SerialPortException e) {
+        	e.printStackTrace();
         	System.out.println("Error while opening the serial port.");
 		}
         catch (IOException e) {
-			System.out.println("Error input/output while initializing the server");
+        	e.printStackTrace();
+			System.out.println("Error input/output while initializing the server (Port 6020 may already be in use)");
+		}
+		finally {
+			try {
+				serialPort.closePort();
+			} catch (SerialPortException e) {
+				e.printStackTrace();
+				System.out.println("Error while closing the serial port.");
+			}
+			try {
+				serv_soc.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("The socket has not been able to close.");
+			}
 		}
     }
 }
