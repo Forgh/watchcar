1d89ea6c196453787aad886e2094248a5572555e
dkurzaj@gmail.com <dkurzaj@gmail.com@cf1d1d16-e840-6ff8-888e-e88bdf6d9097>
2014-04-28 11:00:56 +0000

    Ajout des derniers icône et renommage de WatchYourCar par PiBot

--- ClientFinalExperimentalv2/src/com/watch_your_car/client/AddressActivity.java
+++ ClientFinalExperimentalv2/src/com/watch_your_car/client/AddressActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 public class AddressActivity extends Activity {
@@ -14,6 +15,7 @@ public class AddressActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_address);
 		
 		// load stored data
